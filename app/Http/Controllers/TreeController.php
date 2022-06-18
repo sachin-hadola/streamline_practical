fc3f7f9c865a5178817396ecb5ee1c8d63d3519a
sachin-hadola <155sachin@gmail.com>
2022-06-18 20:41:13 +0530

    remove unused files and code

--- app/Http/Controllers/TreeController.php
+++ app/Http/Controllers/TreeController.php
@@ -6,60 +6,58 @@ use Illuminate\Http\Request;
 use App\Models\Tree;
 use App\Models\Tree_two;
 
-class TreeController extends Controller
-{
-    function index(Request $request)
-    {
+class TreeController extends Controller {
+
+    function index(Request $request) {
 
         $all_data = Tree::where('trees.parent_id', 0)->get();
-
         return view('welcome', compact('all_data'));
     }
-    function getmoredata(Request $request)
-    {
+
+    function getmoredata(Request $request) {
         $childrendata = Tree::where('trees.parent_id', $request->id)->get();
         $html = view('welcome_ajax', compact('childrendata'))->render();
 
         return $html;
     }
-    function savedata(Request $request)
-    {
-        if($request->selection_type == 1){
+
+    function savedata(Request $request) {
+        if ($request->selection_type == 1) {
             $name = $request->name ?? '';
             $request->validate([
                 'name' => 'required'
             ]);
-        }else if($request->selection_type == 2){
+        } else if ($request->selection_type == 2) {
             $request->validate([
                 'file_name' => 'required'
             ]);
         }
-        if($request->has('file_name')){
+        if ($request->has('file_name')) {
             $file = $request->file('file_name');
-   
-            if($request->selection_type == 2){
+
+            if ($request->selection_type == 2) {
                 $name = $file->getClientOriginalName();
             }
             //Move Uploaded File
             $destinationPath = 'uploads';
-            $file->move($destinationPath,$file->getClientOriginalName());
+            $file->move($destinationPath, $file->getClientOriginalName());
         }
 
         $tree = new Tree;
- 
+
         $tree->name = $name ?? '';
         $tree->parent_id = $request->parent_id ?? 0;
         $tree->type = $request->selection_type ?? 1;
 
         $tree->save();
         return 1;
-
     }
-    function deleteddata(Request $request)
-    {
+
+    function deleteddata(Request $request) {
         $id = $request->id;
-        $res=Tree::where('id',$id)->delete();
-        $res=Tree::where('parent_id',$id)->delete();
+        $res = Tree::where('id', $id)->delete();
+        $res = Tree::where('parent_id', $id)->delete();
         return 1;
     }
+
 }
--- app/Models/Tree.php
+++ app/Models/Tree.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tree extends Model
-{
+class Tree extends Model {
+
     use HasFactory;
 
-    public function children()
-    {
+    public function children() {
         return $this->hasMany(Tree::class, 'parent_id', 'id');
     }
+
 }
